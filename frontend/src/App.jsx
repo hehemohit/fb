2d505bc76e6c5916e7c80d68d5c98630f0e77247
hehemohit <mohit9702252229@gmail.com>
2025-10-31 17:05:32 +0530

    hh

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from 'react'
 import './App.css'
 
-const API_BASE = 'http://localhost:4000'
+const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:4000'
 
 function Home() {
   const handleConnect = () => {
